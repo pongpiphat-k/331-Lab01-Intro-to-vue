51a0cfff331d3df10480d26c5517cdc27c61dc79
pongpiphat-ka <pongpiphat_k@cmu.ac.th>
2025-06-27 00:46:12 +0700

    6.7 change inStock status button

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { createApp, ref } = Vue;
 createApp({
     setup() {
         const product = ref('Socks')
-        const description = ref('A pair of warm, fuzzy socks.')
+        const description = ref('This is description.')
         const image = ref('./assets/images/socks_green.jpg')
         const CAMT = ref('https://www.camt.cmu.ac.th/')
         const inStock = ref(true)
@@ -11,10 +11,24 @@ createApp({
         const onsale = ref(true)
         const details = ref(['50% cotton', '30% wool', '20% polyester'])
         const variants = ref([ 
-            { id: 2234, color: 'green'},
-            { id: 2235, color: 'blue'},
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
         ])
         const sizes = ref(['S ', 'M ', 'L '])
+        const cart = ref(0)
+
+        function addToCart() {
+            cart.value += 1
+        }
+
+        function updateImage(variantsImage) {
+            image.value = variantsImage
+        }
+
+        function buyNow() {
+            inStock.value = false
+            inventory.value = 0
+        }
         return {
             product,
             description,
@@ -25,7 +39,11 @@ createApp({
             onsale,
             details,
             variants,
-            sizes
+            sizes,
+            cart,
+            addToCart,
+            updateImage,
+            buyNow
         }
     }
 
