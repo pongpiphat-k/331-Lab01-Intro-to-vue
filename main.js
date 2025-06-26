2c32c32f99387c50d3a51afc4a1e6ad8d4195089
pongpiphat-ka <pongpiphat_k@cmu.ac.th>
2025-06-26 22:38:44 +0700

    commit 3.6

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ createApp({
     setup() {
         const product = ref('Socks')
         const description = ref('A pair of warm, fuzzy socks.')
+        const image = ref('./assets/images/socks_green.jpg')
+        const CAMT = ref('https://www.camt.cmu.ac.th/')
         return {
             product,
-            description
+            description,
+            image,
+            CAMT
         }
     }
 
