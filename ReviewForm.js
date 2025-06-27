2669fc72e54033d593c9685ec5f4edeccb650fbb
pongpiphat-ka <pongpiphat_k@cmu.ac.th>
2025-06-27 09:58:22 +0700

    12.3 using deconstructor

--- ReviewForm.js
+++ ReviewForm.js
@@ -1,18 +1,13 @@
-const { reactive } = Vue;
+const { reactive } = Vue; 
 const reviewForm = {
   template:
     /*html*/
     `<form class="review-form" @submit.prevent="onSubmit">
         <h3>Leave a review</h3>
         <label for="name">Name:</label>
-        <input id="name" v-model="form.name">
-
-        <label for="review">Review:</label>
-        <textarea id="review" v-model="form.review"></textarea>
-
-        <label for="rating">Rating:</label>
-        <select id="rating" v-model="form.rating">
-            <option>5</option>
+        <input id="name" v-model="name"> <label for="review">Review:</label>
+        <textarea id="review" v-model="review"></textarea> <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating"> <option>5</option>
             <option>4</option>
             <option>3</option>
             <option>2</option>
@@ -21,24 +16,22 @@ const reviewForm = {
 
         <label>Would you recommend this product?</label>
         <div style="display: flex; gap: 1em; align-items: center;">
-  <input type="radio" id="recommend-yes" value="Yes" v-model="form.recommend">
-  <label for="recommend-yes">Yes</label>
-  <input type="radio" id="recommend-no" value="No" v-model="form.recommend">
-  <label for="recommend-no">No</label>
-</div>
+            <input type="radio" id="recommend-yes" value="Yes" v-model="recommend"> <label for="recommend-yes">Yes</label>
+            <input type="radio" id="recommend-no" value="No" v-model="recommend"> <label for="recommend-no">No</label>
+        </div>
         
         <input class="button" type="submit" value="Submit">
     </form>`,
     setup(props, { emit }) {
     const form = reactive({
-      name: ' ',
-      review: ' ',
+      name: '', 
+      review: '', 
       rating: null,
       recommend: null
     })
 
     function onSubmit() {
-      if (form.name === ' ' || form.review === ' ' || form.rating === null || form.recommend === null) {
+      if (form.name === '' || form.review === '' || form.rating === null || form.recommend === null) { 
         alert('Please fill out all fields.')
         return
       }
@@ -49,14 +42,15 @@ const reviewForm = {
         recommend: form.recommend
       }
       emit('review-submitted', productReview)
-      form.name = ' '
-      form.review = ' '
+      form.name = '' 
+      form.review = '' 
       form.rating = null
       form.recommend = null
     }
 
     return {
-      form,
+
+      ...toRefs(form),
       onSubmit
     }
   }
--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const { createApp, ref, computed } = Vue;
-
+const { createApp, ref, computed, toRefs } = Vue; 
 const app = createApp({
     setup() {
         const cart = ref([])
@@ -10,11 +9,11 @@ const app = createApp({
         }
 
         function removeFromCart(id) {
-    const index = cart.value.indexOf(id)
-    if (index !== -1) {
-        cart.value.splice(index, 1)
-    }
-}
+            const index = cart.value.indexOf(id)
+            if (index !== -1) {
+                cart.value.splice(index, 1)
+            }
+        }
 
         return {
             cart,
